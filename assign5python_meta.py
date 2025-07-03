7357597f25ec7b3942ce80aa758ec3cc90263db6
Bushra Saeed <bushrasaeed036@gmail.com>
2025-07-03 22:02:03 +0500

    Add files via upload

---
+++ assign5python_meta.py
@@ -0,0 +1,31 @@
+# It will print the list of fruits 
+fruits = ["cherry", "peach", "berries"]
+print("Fruits list:", fruits)
+
+# It will print the second fruit of the list
+print("Second fruit:", fruits[1])
+
+# It will add a new fruit to the list
+fruits.append("grapes")
+
+# It will print the updated list
+print("Updated fruits list:", fruits)
+
+
+
+# It will print the dictionary of student
+student = {
+"Name": "Bushra",
+"Age": 20,
+"Grade": "10th"
+}
+print("Student Info:", student)
+
+# It will print the name value
+print("Name:", student["Name"])
+
+# It will add a new key-value pair
+student["School"] = "Happy Home School"
+
+# It will print the updated dictionary
+print("Updated Student Info:", student)
